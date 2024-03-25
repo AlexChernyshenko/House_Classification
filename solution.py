6a4c76546d4010afd74e3715b45fc9623d62fca1
Oleksii Chernyshenko <106983386+AlexChernyshenko@users.noreply.github.com>
2024-03-25 18:35:41 +0100

    Task 2

---
+++ solution.py
@@ -0,0 +1,32 @@
+import os
+import requests
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+if __name__ == '__main__':
+    if not os.path.exists('../Data'):
+        os.mkdir('../Data')
+
+    # Download data if it is unavailable.
+    if 'house_class.csv' not in os.listdir('../Data'):
+        sys.stderr.write("[INFO] Dataset is loading.\n")
+        url = "https://www.dropbox.com/s/7vjkrlggmvr5bc1/house_class.csv?dl=1"
+        r = requests.get(url, allow_redirects=True)
+        open('../Data/house_class.csv', 'wb').write(r.content)
+        sys.stderr.write("[INFO] Loaded.\n")
+
+    # write your code here
+    df = pd.read_csv('../Data/house_class.csv')
+
+    # print(f'''{df.shape[0]}
+    # {df.shape[1]}
+    # {df.isna().any().sum() != 0}
+    # {df.Room.max()}
+    # {df.Area.mean()}
+    # {df.Zip_loc.nunique()}
+    # ''')
+    X, y = df.iloc[:, 1:7], df['Price']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=1,
+                                                        stratify=X['Zip_loc'].values)
+    print(dict(X_train["Zip_loc"].value_counts()))
